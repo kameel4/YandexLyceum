3f152193e7674cc18a39e8ca2363a362caef1be6
kamil <kamilshaimiev@yandex.ru>
2020-11-19 23:15:03 +0500

    (◕‿◕)понял че писать, стало проще

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import sys, random
 
-from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
 
-class Example(QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(452, 365)
+        self.btn = QtWidgets.QPushButton(Form)
+        self.btn.setGeometry(QtCore.QRect(150, 150, 111, 41))
+        self.btn.setObjectName("btn")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.btn.setText(_translate("Form", "кнопка да"))
+
+
+class Example(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.initUI()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.btn.clicked.connect(self.paint)
-
 
     def initUI(self):
         self.setWindowTitle('Рисование')
@@ -31,7 +47,8 @@ class Example(QWidget):
     def draw_flag(self, qp):
         x, y = random.randint(0, 200), random.randint(0, 200)
         x1, y1 = x, y
-        qp.setBrush(QColor(255, 255, 0))
+        r, g, b = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+        qp.setBrush(QColor(r, g, b))
         qp.drawEllipse(x, y, x1, y1)
 
 
